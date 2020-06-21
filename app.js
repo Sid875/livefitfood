83090c6def60f7c6d2e1d49f078bdc3899a11596
sprajapati11 <sprajapati11@myseneca.ca>
2020-06-22 00:17:30 +0300

    Registration Setup

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 
 const handlebars = require("express-handlebars");
+// environment varaibles
+require("dotenv").config();
+
 const modelData = require("./models/data");
-const e = require("express");
+const mail = require("./services/mail");
 
 app.use(express.static("public"));
 
@@ -106,6 +109,75 @@ app.post("/login", function (req, res) {
 // REGISTER
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  var usernameError = false;
+  var usernameErrorMessage = "";
+  var passwordError = false;
+  var passwordErrorMessage = "";
+  var emailError = false;
+  var emailErrorMessage = "";
+
+  if (username) {
+    usernameReg = RegExp(/^[a-z0-9_-]{4,12}$/);
+    if (!usernameReg.test(username)) {
+      usernameError = true;
+      usernameErrorMessage =
+        "Username must be between 4-12 characters & Can only contain letters, numbers, dashes or underscores!";
+    }
+  } else {
+    usernameError = true;
+    usernameErrorMessage = "Username cannot be blank!";
+  }
+
+  if (password) {
+    passwordReg = RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
+    if (!passwordReg.test(password)) {
+      passwordError = true;
+      passwordErrorMessage =
+        "Password must contain a minimum eight characters, at least one letter and one number!";
+    }
+  } else {
+    passwordError = true;
+    passwordErrorMessage = "Password cannot be blank!";
+  }
+
+  if (email) {
+    emailReg = RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm
+    );
+    if (!emailReg.test(email)) {
+      emailError = true;
+      emailErrorMessage = "Invalid email!";
+    }
+  } else {
+    emailError = true;
+    emailErrorMessage = "Email cannot be blank!";
+  }
+
+  // Handle Errors
+  if (usernameError || passwordError || emailError) {
+    res.render("register", {
+      extraFormClasses: "invalid-form",
+      username: username,
+      usernameError: usernameError,
+      usernameErrorMessage: usernameErrorMessage,
+      email: email,
+      emailError: emailError,
+      emailErrorMessage: emailErrorMessage,
+      password: password,
+      passwordError: passwordError,
+      passwordErrorMessage: passwordErrorMessage,
+    });
+  } else {
+    mail.sendEmail(email);
+    res.redirect("/");
+  }
+  //
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
